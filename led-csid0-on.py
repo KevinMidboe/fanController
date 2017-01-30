e1fb7759c8c555965a478b4e90ed147f11382b89
KevinMidboe <kevin.midboe@gmail.com>
2017-01-30 22:53:47 +0100

    Cleaned up in the files. Removed duplicated web files

--- led-csid0-on.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import CHIP_IO.GPIO as GPIO     #import the GPIO library
-
-GPIO.setup("CSID0", GPIO.OUT)   #set CSID0 as an output
-GPIO.output("CSID0", GPIO.HIGH) #set CSID0 (LED) HIGH (On)
-
-f = open( 'public/state.txt', 'w' )
-f.write('on')
-f.close()
