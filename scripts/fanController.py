0cb52876a1dd419e7fb7e5302d380258866d864f
KevinMidboe <kevin.midboe@gmail.com>
2017-02-19 11:55:48 +0100

    Changed the fanToggling to only be done by one script, changed server.js to reflect these changes and also needed to do some changes to index.html. These being checking the return text and some linting.

---
+++ scripts/fanController.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import CHIP_IO.GPIO as GPIO     #import the GPIO library
+import sqlite3
+from sys import argv
+
+path = "scripts/fanState.db"
+
+def updateFanstate(state):
+	conn = sqlite3.connect(path)
+	c = conn.cursor()
+	c.execute('UPDATE fanstate SET state='+str(state))
+
+	conn.commit()
+	conn.close()
+
+def getFanstate():
+	conn = sqlite3.connect(path)
+	c = conn.cursor()
+
+	c.execute('SELECT state FROM fanstate')
+
+	state = c.fetchone()[0]
+	conn.close()
+
+	return state
+
+def turnFanON():
+	if not getFanstate():
+		updateFanstate(1)
+		GPIO.setup("CSID0", GPIO.OUT)   #set CSID0 as an output
+		GPIO.output("CSID0", GPIO.HIGH) #set CSID0 (LED) HIGH (On)
+		return '1'
+
+def turnFanOFF():
+	if getFanstate():
+		updateFanstate(0)
+		GPIO.setup("CSID0", GPIO.OUT)   #set CSID0 as an output
+		GPIO.output("CSID0", GPIO.LOW) #set CSID0 (LED) HIGH (On)
+		return '1'
+
+
+if __name__ == '__main__':
+	arg = argv[1]
+	if (arg == 'on'):
+		print(turnFanON())
+	elif (arg == 'off'):
+		print(turnFanOFF())
+	elif (arg == 'get'):
+		print(getFanstate())
+	else:
+		print("Invalid input")
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var path = require('path');
 var PythonShell = require('python-shell');
 
 app.use(express.static('public'));
-
-var fs = require('fs');
  
 
 // This responds with "Hello World" on the homepage
@@ -16,31 +14,52 @@ app.get('/', function (req, res) {
 })
 
 app.get('/state', function(req, res) {
-	var contents = fs.readFileSync('public/state.txt', 'utf8');
-	console.log(contents);
-	res.send(contents);
-	
+	var options = {
+	  pythonOptions: ['-u'],
+	  args: 'get'
+	};
+
+	PythonShell.run('scripts/fanController.py', options, function (err, results) {
+  	if (err) throw err;
+
+  	if (results[0] == true)
+  		res.send('on')
+  	else
+  		res.send('off')
+	});
 })
 
 // This responds a POST request for the homepage
 app.post('/on', function (req, res) {
-	console.log("Got a POST request for ON");
+	var options = {
+	  pythonOptions: ['-u'],
+	  args: 'on'
+	};
 
-	PythonShell.run('pyscripts/led-csid0-on.py', function (err) {
-	if (err) throw err;
-		console.log('finished');
+	PythonShell.run('scripts/fanController.py', options, function (err, results) {
+  	if (err) throw err;
+  	
+  	if (results[0] == true)
+  		res.send('on')
+  	else
+  		res.send('none')
 	});
-	res.send('on');
 })
 
 app.post('/off', function (req, res) {
-	console.log("Got a POST request for OFF");
+	var options = {
+	  pythonOptions: ['-u'],
+	  args: 'off'
+	};
 
-	PythonShell.run('pyscripts/led-csid0-off.py', function (err) {
-	if (err) throw err;
-		console.log('finished');
+	PythonShell.run('scripts/fanController.py', options, function (err, results) {
+  	if (err) throw err;
+  	
+  	if (results[0] == true)
+  		res.send('off')
+  	else
+  		res.send('none')
 	});
-	res.send('off');
 })
 
 var server = app.listen(3000, function () {
