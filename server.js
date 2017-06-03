7558afc444efde460fa18803de2598cfb894a2ab
KevinMidboe <kevin.midboe@gmail.com>
2017-06-03 18:58:55 +0200

    Changed so that the fanstate is reloaded when application starts

--- server.js
+++ server.js
@@ -66,5 +66,11 @@ var server = app.listen(3000, function () {
 	var host = server.address().address
 	var port = server.address().port
 
-	console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Example app listening at http://%s:%s", host, port);
+	console.log('Setting fan to previous state');
+	var options = {
+	  pythonOptions: ['-u'],
+	  args: 'reload'
+	};
+	PythonShell.run('scripts/fanController.py', options);
 })
